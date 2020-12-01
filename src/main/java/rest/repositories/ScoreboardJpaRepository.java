82c66e1d511e1e2cd56c2f722d428496ddd4ea73
Maxim <maximvanesch15@gmail.com>
2020-12-01 10:32:19 +0100

    changes for API

--- src/main/java/rest/repositories/ScoreboardJpaRepository.java
+++ src/main/java/rest/repositories/ScoreboardJpaRepository.java
@@ -3,7 +3,6 @@ package rest.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import models.ScoreboardItem;
 
-public interface ScoreboardJpaRepository extends JpaRepository<ScoreboardItem, Integer> {
 import java.util.List;
 
 public interface ScoreboardJpaRepository extends JpaRepository<ScoreboardItem, Integer> {
